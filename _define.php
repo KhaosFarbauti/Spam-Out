3af8bb23b14222e0a3fc9e594b9bcfc31357c2d7
Khaos Farbauti Ibn Oblivion <propserv@hotmail.com>
2015-04-15 14:31:14 +0200

    Licence change

--- _define.php
+++ _define.php
@@ -1,21 +1,25 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
+#   Copyright 2015 Khaos Farbauti Ibn Oblivion
 #
-# This file is part of Spam Out, a plugin for DotClear2.
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
 #
-# Copyright (c) 2008 Khaos Farbauti Ibn Oblivion and contributors
-# Licensed under the GPL version 2.0 license.
-# See LICENSE file or
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#       http://www.apache.org/licenses/LICENSE-2.0
 #
-# -- END LICENSE BLOCK ------------------------------------
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 if (!defined('DC_RC_PATH')) { return; }
  
 $this->registerModule(
   /* Name */           "Spam Out",
   /* Description*/     "Interdire les spammeurs sur votre blog à l'aide de RBL",
   /* Author */         "Khaos Farbauti Ibn Oblivion",
-  /* Version */        '1.2',
+  /* Version */        '1.3',
 	/* Permissions */		 'usage,contentadmin'
 );
 ?>
--- locales/en/resources.php
+++ locales/en/resources.php
@@ -1,14 +1,18 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
+#   Copyright 2015 Khaos Farbauti Ibn Oblivion
 #
-# This file is part of Spam Out, a plugin for DotClear2.
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
 #
-# Copyright (c) 2008 Khaos Farbauti Ibn Oblivion and contributors
-# Licensed under the GPL version 2.0 license.
-# See LICENSE file or
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#       http://www.apache.org/licenses/LICENSE-2.0
 #
-# -- END LICENSE BLOCK ------------------------------------
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 if (!isset($__resources['help']['spamout']))
 {
 	$__resources['help']['spamout'] = dirname(__FILE__).'/help/spamout.html';
